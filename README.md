34dfe88ff8563de0cec240885a4a712ecd284e61
Austin Caron <aus10caron@icloud.com>
2019-05-29 00:07:48 -0400

    Webinterface

--- README.md
+++ README.md
@@ -15,6 +15,6 @@ $ npm run test
 
 ### Features
 
-Currently: Adding a timer to send a template webhook. Basic webhook with example information.
+Currently: Decided to go with using a web interface running on localhost, rather than electron.
 
-Working on: Deciding wether or not I want to put this into an electron app or keep it as an tool. If I put this into electron, I will most likely include an embed builder in there as well.
+Working on: Actually filling out the web page.
--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const webhookHandler = require("discord-webhook-api");
+const express = require("express");
 const config = require("./config.json");
 
 const newWebhook = new webhookHandler();
 
-const sendToDiscord = delay => {
+// Initialize app
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Set Static Folder
+app.use(express.static(`${__dirname}/public`));
+
+// use setTimeout to create a delay webhook
+const sendToDiscord = (delay, title, description, image, color) => {
   console.log("delay is:", delay);
 
   setTimeout(() => {
     newWebhook
-      .addTitle("Test title")
-      .addDescription("Test description")
-      .addImageUrl("https://pbs.twimg.com/media/D6KdDkWXkAIWaLO.jpg")
-      .addColor("#F00000")
+      .addTitle(title || "Test title")
+      .addDescription(description || "Test description")
+      .addImageUrl(image || "https://pbs.twimg.com/media/D6KdDkWXkAIWaLO.jpg")
+      .addColor(color || "#F00000")
       .addTimestamp()
       .sendTo(config.webhook);
   }, delay);
 };
 
-sendToDiscord(10000);
+// Run function
+// sendToDiscord(10000);
+
+// Listens for a port to use
+app.listen(port, () => {
+  console.log(`Server started on ${port}`);
+});
