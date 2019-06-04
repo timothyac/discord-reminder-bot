bf63205081704261f97b01464d3793e5121cafb1
Austin Caron <aus10caron@icloud.com>
2019-06-03 22:38:59 -0400

    Working version

--- public/js/main.js
+++ public/js/main.js
@@ -4,14 +4,64 @@ const config = require("./config.json");
 
 const newWebhook = new webhookHandler();
 
-const submitButton = document.querySelector("#submit-input");
+const submitButton = document.querySelector("#submit-button");
+const webhookInput = document.querySelector("#webhook-input");
+const colorInput = document.querySelector("#color-input");
+const delayInput = document.querySelector("#delay-input");
+const titleInput = document.querySelector("#title-input");
+const descriptionInput = document.querySelector("#description-input");
+const fileAttachment = document.querySelector("#file-attachment");
 
+// Run refresh command to load products on init
+window.onload = onLoad;
+
+// Listen for click
 submitButton.addEventListener("click", sendToDiscord);
 
+// run this on load
+function onLoad() {
+  let oldWebhook = JSON.parse(localStorage.getItem("webhook"));
+  let oldColor = JSON.parse(localStorage.getItem("color"));
+  let oldDelay = JSON.parse(localStorage.getItem("delay"));
+
+  if (oldWebhook) {
+    webhookInput.value = oldWebhook;
+  }
+
+  if (oldColor) {
+    colorInput.value = oldColor;
+  }
+
+  if (oldDelay) {
+    delayInput.value = oldDelay;
+  }
+}
+
 // use setTimeout to create a delay webhook
-function sendToDiscord(e, delay, title, description, image, color) {
+function sendToDiscord(e) {
   e.preventDefault();
-  console.log("delay is:", delay);
+
+  let webhook = webhookInput.value;
+  let title = titleInput.value;
+  let color = colorInput.value;
+  let delay = delayInput.value;
+  let description = descriptionInput.value;
+  let image = "https://pbs.twimg.com/media/D6KdDkWXkAIWaLO.jpg";
+
+  if (
+    webhook == "" ||
+    title == "" ||
+    color == "" ||
+    delay == "" ||
+    description == ""
+  ) {
+    alert("Missing info");
+    return;
+  }
+
+  localStorage.setItem("webhook", JSON.stringify(webhook));
+  localStorage.setItem("color", JSON.stringify(color));
+  localStorage.setItem("delay", JSON.stringify(delay));
 
   setTimeout(() => {
     newWebhook
@@ -20,7 +70,7 @@ function sendToDiscord(e, delay, title, description, image, color) {
       .addImageUrl(image || "https://pbs.twimg.com/media/D6KdDkWXkAIWaLO.jpg")
       .addColor(color || "#F00000")
       .addTimestamp()
-      .sendTo(config.webhook);
+      .sendTo(webhook);
   }, delay);
 }
 
