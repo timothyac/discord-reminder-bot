cf79e83e5723ff939f793641dd86dd4dea2215de
Austin Caron <aus10caron@icloud.com>
2019-06-04 14:07:22 -0400

    Moving logic back serverside. Trying to figure out image upload

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require("path");
+
+const upload = multer();
 
 // Initialize app
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Set Static Folder
+// Set up middleware and src
 app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array()); 
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.statusCode = 200
+  res.redirect('/')
+})
 
 // Listens for a port to use
 app.listen(port, () => {
